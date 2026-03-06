4e564fa57f2a5dcf7178677a537f9610896f9e75
itachixd12 <itachixd12@example.com>
2026-03-05 19:42:38 -0500

    Actualizar entidad Usuario

--- app/src/main/java/com/example/citasmedicas/data/entity/UsuarioEntity.kt
+++ app/src/main/java/com/example/citasmedicas/data/entity/UsuarioEntity.kt
@@ -1,4 +1,15 @@
-package com.example.citasmedicas.data.entity
+package com.estalin.citasmedicasapp.data.entity
 
-class UsuarioEntity {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "usuarios")
+data class UsuarioEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val nombre: String,
+    val email: String,
+    val password: String,
+    val rol: String,
+    val especialidad: String? = null // Campo nuevo para médicos
+)
