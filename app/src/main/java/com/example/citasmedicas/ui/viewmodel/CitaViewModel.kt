21523594ac2d87f7f8b9b34871922be574593d89
estalin-2006 <estalintenelema4@gmail.com>
2026-03-07 20:16:38 -0500

    App completa

--- app/src/main/java/com/example/citasmedicas/ui/viewmodel/CitaViewModel.kt
+++ app/src/main/java/com/example/citasmedicas/ui/viewmodel/CitaViewModel.kt
@@ -1,4 +1,25 @@
 package com.example.citasmedicas.ui.viewmodel
 
-class CitaViewModel {
+import androidx.lifecycle.*
+import com.example.citasmedicas.data.AppRepository
+import com.example.citasmedicas.data.entity.CitaEntity
+import kotlinx.coroutines.launch
+
+class CitaViewModel(private val repository: AppRepository) : ViewModel() {
+
+    val allCitas: LiveData<List<CitaEntity>> = repository.allCitas
+
+    fun insert(cita: CitaEntity) = viewModelScope.launch {
+        repository.insertCita(cita)
+    }
+}
+
+class CitaViewModelFactory(private val repository: AppRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(CitaViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return CitaViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
